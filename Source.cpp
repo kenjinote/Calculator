8320ab3140329f7eb17ebd11a4f61314214216a5
kenjinote <2605401+kenjinote@users.noreply.github.com>
2022-07-26 00:50:54 +0900

    入力された値をそのまま出力する

--- Source.cpp
+++ Source.cpp
@@ -2,103 +2,65 @@
 
 #include <windows.h>
 
-#define DEFAULT_DPI 96
-#define SCALEX(X) MulDiv(X, uDpiX, DEFAULT_DPI)
-#define SCALEY(Y) MulDiv(Y, uDpiY, DEFAULT_DPI)
-#define POINT2PIXEL(PT) MulDiv(PT, uDpiY, 72)
+TCHAR szClassName[] = TEXT("Calculator");
 
-TCHAR szClassName[] = TEXT("Window");
+WNDPROC EditWndProc;
 
-BOOL GetScaling(HWND hWnd, UINT* pnX, UINT* pnY)
+LRESULT CALLBACK EditProc1(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
-	BOOL bSetScaling = FALSE;
-	const HMONITOR hMonitor = MonitorFromWindow(hWnd, MONITOR_DEFAULTTONEAREST);
-	if (hMonitor)
+	switch (msg)
 	{
-		HMODULE hShcore = LoadLibrary(TEXT("SHCORE"));
-		if (hShcore)
-		{
-			typedef HRESULT __stdcall GetDpiForMonitor(HMONITOR, int, UINT*, UINT*);
-			GetDpiForMonitor* fnGetDpiForMonitor = reinterpret_cast<GetDpiForMonitor*>(GetProcAddress(hShcore, "GetDpiForMonitor"));
-			if (fnGetDpiForMonitor)
-			{
-				UINT uDpiX, uDpiY;
-				if (SUCCEEDED(fnGetDpiForMonitor(hMonitor, 0, &uDpiX, &uDpiY)) && uDpiX > 0 && uDpiY > 0)
-				{
-					*pnX = uDpiX;
-					*pnY = uDpiY;
-					bSetScaling = TRUE;
-				}
+	case WM_CHAR:
+		if (wParam == 0x0D) {
+			HWND hParent = GetParent(hWnd);
+			if (hParent) {
+				PostMessage(hParent, WM_COMMAND, IDOK, 0);
 			}
-			FreeLibrary(hShcore);
+			return 0;
 		}
+		break;
+	default:
+		break;
 	}
-	if (!bSetScaling)
-	{
-		HDC hdc = GetDC(NULL);
-		if (hdc)
-		{
-			*pnX = GetDeviceCaps(hdc, LOGPIXELSX);
-			*pnY = GetDeviceCaps(hdc, LOGPIXELSY);
-			ReleaseDC(NULL, hdc);
-			bSetScaling = TRUE;
-		}
-	}
-	if (!bSetScaling)
-	{
-		*pnX = DEFAULT_DPI;
-		*pnY = DEFAULT_DPI;
-		bSetScaling = TRUE;
-	}
-	return bSetScaling;
+	return CallWindowProc(EditWndProc, hWnd, msg, wParam, lParam);
 }
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
 	static HWND hButton;
-	static HWND hEdit;
+	static HWND hInputEdit;
+	static HWND hOutputEdit;
 	static HFONT hFont;
-	static UINT uDpiX = DEFAULT_DPI, uDpiY = DEFAULT_DPI;
 	switch (msg)
 	{
 	case WM_CREATE:
-		hButton = CreateWindow(TEXT("BUTTON"), TEXT("変換"), WS_VISIBLE | WS_CHILD, 0, 0, 0, 0, hWnd, (HMENU)IDOK, ((LPCREATESTRUCT)lParam)->hInstance, 0);
-		hEdit = CreateWindowEx(WS_EX_CLIENTEDGE, TEXT("EDIT"), 0, WS_VISIBLE | WS_CHILD | ES_MULTILINE | ES_AUTOHSCROLL | ES_AUTOVSCROLL, 0, 0, 0, 0, hWnd, 0, ((LPCREATESTRUCT)lParam)->hInstance, 0);
-		SendMessage(hWnd, WM_DPICHANGED, 0, 0);
+		hButton = CreateWindow(TEXT("BUTTON"), TEXT("計算"), WS_VISIBLE | WS_CHILD, 0, 0, 0, 0, hWnd, (HMENU)IDOK, ((LPCREATESTRUCT)lParam)->hInstance, 0);
+		hInputEdit = CreateWindowEx(WS_EX_CLIENTEDGE, TEXT("EDIT"), 0, WS_VISIBLE | WS_CHILD | ES_AUTOHSCROLL, 0, 0, 0, 0, hWnd, 0, ((LPCREATESTRUCT)lParam)->hInstance, 0);
+		hOutputEdit = CreateWindowEx(WS_EX_CLIENTEDGE, TEXT("EDIT"), 0, WS_VISIBLE | WS_CHILD | WS_VSCROLL| ES_READONLY | ES_MULTILINE | ES_AUTOHSCROLL | ES_AUTOVSCROLL, 0, 0, 0, 0, hWnd, 0, ((LPCREATESTRUCT)lParam)->hInstance, 0);
+		SendMessage(hButton, WM_SETFONT, (WPARAM)hFont, 0);
+		SendMessage(hInputEdit, WM_SETFONT, (WPARAM)hFont, 0);
+		SendMessage(hOutputEdit, WM_SETFONT, (WPARAM)hFont, 0);
+		SetFocus(hInputEdit);
+		EditWndProc = (WNDPROC)SetWindowLongPtr(hInputEdit, GWLP_WNDPROC, (LONG_PTR)EditProc1);
 		break;
 	case WM_SIZE:
-		MoveWindow(hButton, POINT2PIXEL(10), POINT2PIXEL(10), POINT2PIXEL(256), POINT2PIXEL(32), TRUE);
-		MoveWindow(hEdit, POINT2PIXEL(10), POINT2PIXEL(50), LOWORD(lParam) - POINT2PIXEL(20), HIWORD(lParam) - POINT2PIXEL(60), TRUE);
+		MoveWindow(hButton, LOWORD(lParam) - 128, HIWORD(lParam) - 60, 128, 60, TRUE);
+		MoveWindow(hInputEdit, 0, HIWORD(lParam) - 60, LOWORD(lParam) - 128, 60, TRUE);
+		MoveWindow(hOutputEdit, 0, 0, LOWORD(lParam), HIWORD(lParam) - 60, TRUE);
 		break;
 	case WM_COMMAND:
 		if (LOWORD(wParam) == IDOK)
 		{
-			SetWindowText(hEdit, 0);
-			TCHAR szText[1024];
-			wsprintf(szText, TEXT("%d"), GetTickCount());
-			SendMessage(hEdit, EM_REPLACESEL, 0, (LPARAM)szText);
+			DWORD dwTextLength = GetWindowTextLength(hInputEdit);
+			LPWSTR lpszText = (LPWSTR)GlobalAlloc(0, sizeof(WCHAR) * (dwTextLength + 1));
+			(void)GetWindowText(hInputEdit, lpszText, dwTextLength + 1);
+			dwTextLength = (DWORD)SendMessage(hOutputEdit, WM_GETTEXTLENGTH, 0, 0);
+			SendMessage(hOutputEdit, EM_SETSEL, (WPARAM)dwTextLength, (LPARAM)dwTextLength);
+			SendMessage(hOutputEdit, EM_REPLACESEL, 0, (LPARAM)lpszText);
+			SendMessage(hOutputEdit, EM_REPLACESEL, 0, (LPARAM)L"\r\n");
+			GlobalFree(lpszText);
+			SetWindowText(hInputEdit, 0);
 		}
-		break;
-	case WM_NCCREATE:
-		{
-			const HMODULE hModUser32 = GetModuleHandle(TEXT("user32.dll"));
-			if (hModUser32)
-			{
-				typedef BOOL(WINAPI*fnTypeEnableNCScaling)(HWND);
-				const fnTypeEnableNCScaling fnEnableNCScaling = (fnTypeEnableNCScaling)GetProcAddress(hModUser32, "EnableNonClientDpiScaling");
-				if (fnEnableNCScaling)
-				{
-					fnEnableNCScaling(hWnd);
-				}
-			}
-		}
-		return DefWindowProc(hWnd, msg, wParam, lParam);
-	case WM_DPICHANGED:
-		GetScaling(hWnd, &uDpiX, &uDpiY);
-		DeleteObject(hFont);
-		hFont = CreateFontW(-POINT2PIXEL(10), 0, 0, 0, FW_NORMAL, 0, 0, 0, SHIFTJIS_CHARSET, 0, 0, 0, 0, L"MS Shell Dlg");
-		SendMessage(hButton, WM_SETFONT, (WPARAM)hFont, 0);
-		SendMessage(hEdit, WM_SETFONT, (WPARAM)hFont, 0);
 		break;
 	case WM_DESTROY:
 		DeleteObject(hFont);
@@ -110,7 +72,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 	return 0;
 }
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPreInst, LPSTR pCmdLine, int nCmdShow)
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPreInst, LPWSTR pCmdLine, int nCmdShow)
 {
 	MSG msg;
 	WNDCLASS wndclass = {
@@ -128,7 +90,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPreInst, LPSTR pCmdLine, int 
 	RegisterClass(&wndclass);
 	HWND hWnd = CreateWindow(
 		szClassName,
-		TEXT("Window"),
+		TEXT("Calculator"),
 		WS_OVERLAPPEDWINDOW | WS_CLIPCHILDREN,
 		CW_USEDEFAULT,
 		0,
